b4dad6a10847ce65d2aaf419fcdc954f535042ac
Mishal Nawaz <112049216+Mishn1@users.noreply.github.com>
2023-09-22 02:29:58 -0400

    Add files via upload

---
+++ homework#2.R
@@ -0,0 +1,14 @@
+#Homework 2
+#Statistics and Introduction to Econometrics
+
+#Group member: Mishal Nawaz, Vanessa Crowe and Akash Persaud
+
+#PP1 analysis:
+If we get a six after we roll it once, we conclude that the dice is not fair because the probablity of getting the dice is 1/6, so it woulf be considered unfair.
+
+#PP2 analysis:
+If six shows up 3 times in every twenty roll, the dice will be fair, but if there are more in six, more then 6 times when rolled twenty times, the it would be judged as unfair. Though with one of our group member vanessa, her rice would either only land on one or six, so getting a six would be unfair as she was able to get six, 14 times out of twenty.
+
+#PP3
+If six shows up 6 times, when we roll it hundred times, the. the dice will be concluded not fair. the probablity of rolling the dice hundred times, and getting a six would be [(1/6)^100]. it will give us a low probablity which is why we conclude that it will be fair. But if the dice get 60 six's while being rolled hundred time, then the probablity will be very high, thus concluded unfair.
+
